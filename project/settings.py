da092eb0d3efa5f19b701c0483e9842717da12ef
Yeabsira Ashenafi <ashenafiyeabsira@gmail.com>
2023-03-06 15:42:46 +0300

    added environ package

--- project/settings.py
+++ project/settings.py
@@ -1,6 +1,12 @@
 
 from pathlib import Path
 
+import os,environ
+
+env = environ.Env()
+
+environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
