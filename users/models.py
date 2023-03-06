27a84a93932314015aadeb5763425cca34e95a8d
Yeabsira Ashenafi <ashenafiyeabsira@gmail.com>
2023-03-06 15:37:45 +0300

    added a users app

---
+++ users/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contirb.auth.models import AbstractUser
+
+# Create your models here.
+
+class ExtendedUser(AbstractUser):
+
+    phone = models.CharField(max_length=25)
+    email = models.EmailField(max_length=255,verbose_name='email')
+    email_verified = models.BooleanField(default=False)
+    is_verified = models.BooleanField(default=False)
